47979d66aabae809d55fda276865a2ab6d27f0ef
binohead <68013604+binohead@users.noreply.github.com>
2021-04-28 15:20:18 -0300

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Andy Horn',
+    site_url: 'https://andysbirdblog.vercel.app/',
+    feed_url: 'https://andysbirdblog.vercel.app/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
